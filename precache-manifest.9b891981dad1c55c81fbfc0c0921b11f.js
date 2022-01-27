18c2d7614575fe2e8095a6f25e7ec4d642872a1d
Shreesha Bhat <54950151+Shreesha5102@users.noreply.github.com>
2022-01-27 18:57:48 +0530

    Updates

---
+++ precache-manifest.9b891981dad1c55c81fbfc0c0921b11f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03fc98eb5673ac019f5e088f427e4e1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c810469d07426f2eab9",
+    "url": "/static/css/2.dbd8b1e0.chunk.css"
+  },
+  {
+    "revision": "0fd34cbe5605fa21c16d",
+    "url": "/static/css/main.b7b1e8d2.chunk.css"
+  },
+  {
+    "revision": "2c810469d07426f2eab9",
+    "url": "/static/js/2.f34e6d79.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.f34e6d79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fd34cbe5605fa21c16d",
+    "url": "/static/js/main.9caba631.chunk.js"
+  },
+  {
+    "revision": "12ed1685f73ec57702f7",
+    "url": "/static/js/runtime-main.35a91ad6.js"
+  },
+  {
+    "revision": "79863588504030a6b9c0aa21d5e15e31",
+    "url": "/static/media/HPE-logo.79863588.png"
+  },
+  {
+    "revision": "547bf604e7beed3c7a05beb9089482e7",
+    "url": "/static/media/shreesha_profile_pic.547bf604.jpg"
+  }
+]);
